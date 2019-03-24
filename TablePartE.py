405efa9ecdcc3547e740673955043e8a2167ce5c
lennyGiannachi <lennygiannachi@gmail.com>
2019-03-24 18:31:38 -0400

    Update TablePartE.py

--- TablePartE.py
+++ TablePartE.py
@@ -7,7 +7,7 @@ conn = hb.Connection()
 table = conn.table('powers')
 column = (b'personal:hero', b'personal:power', b'professional:name', b'professional:xp', b'custom:color')
 
-for key, data in table.scan(columns=column, include_timestamp=True)
+for key, data in table.scan(columns=column, include_timestamp=True:
     print('Found: {}, {}'.format(key, data))
 
 conn.close()
