f759a1542aa57c85cbd68c5ebb2b5771a6e63715
lennyGiannachi <lennygiannachi@gmail.com>
2019-03-24 18:24:43 -0400

    Update TablePartE.py

--- TablePartE.py
+++ TablePartE.py
@@ -3,6 +3,11 @@ import happybase as hb
 # DON'T CHANGE THE PRINT FORMAT, WHICH IS THE OUTPUT
 # OR YOU WON'T RECEIVE POINTS FROM THE GRADER
 
-for key, data in ???:
+conn = hb.Connection()
+table = conn.table('powers')
+column = (b'personal:hero', b'personal:power', b'professional:name', b'professional:xp', b'custom:color')
+
+for key, data in table.scan(columns=column, include_timestamp=True)
     print('Found: {}, {}'.format(key, data))
 
+conn.close()
