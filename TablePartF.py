d09686b4b7fa1c6a708f6135865af6b48bb2b341
lennyGiannachi <lennygiannachi@gmail.com>
2019-03-24 18:53:47 -0400

    Update TablePartF.py

--- TablePartF.py
+++ TablePartF.py
@@ -2,15 +2,33 @@ import happybase as hb
 
 # DON'T CHANGE THE PRINT FORMAT, WHICH IS THE OUTPUT
 # OR YOU WON'T RECEIVE POINTS FROM THE GRADER
+# Select p.name, p.power, p1.name, p1.power, p.color
+# From powers p
+# Inner Join powers p1
+# On p.color = p1.color And p.name <> p1.name
 
-color = ???
-name = ???
-power = ???
+conn = hb.Connection() 
+table = conn.table('powers') 
 
-color1 = ???
-name1 = ???
-power1 = ???
-
-print('{}, {}, {}, {}, {}'.format(name, power, name1, power1, color))
+row_num = 1
+while True:
+  row = table.row(encode('row'+str(row_num)))
+  color = row[b'custom:color']
+  name = row[b'professional:name']
+  power = personal[b'personal:power']
+  row_num1 = 1
+  while True:
+    row = table.row(encode('row'+str(row_num1)))
+    color = row[b'custom:color']
+    name = row[b'professional:name']
+    power = personal[b'personal:power']
+    
+    if color == color1 and name != name2:
+      print('{}, {}, {}, {}, {}'.format(name, power, name1, power1, color))
+    row_num1 += 1
+  row_num += 1
+  
+conn.close()
+      
 
 
