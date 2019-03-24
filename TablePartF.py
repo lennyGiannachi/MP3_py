c932d1081778dbba734ecd753ee78a9fae63f112
lennyGiannachi <lennygiannachi@gmail.com>
2019-03-24 19:07:14 -0400

    Update TablePartF.py

--- TablePartF.py
+++ TablePartF.py
@@ -9,16 +9,16 @@ import happybase as hb
 
 conn = hb.Connection() 
 table = conn.table('powers') 
-
-row_num = 1
-while True:
+rows = count(table)
+row_num = 0
+while row_num < rows:
   row = 'row'+str(row_num)
   row = table.row(str.encode(row))
   color = row[b'custom:color']
   name = row[b'professional:name']
   power = row[b'personal:power']
   row_num1 = 1
-  while True:
+  while row_num1 < rows:
     row = 'row'+str(row_num1)
     row = table.row(str.encode(row))
     color1 = row[b'custom:color']
