441b95293447cd8bd2bc1ef21d7da469c1fce7ce
lennyGiannachi <lennygiannachi@gmail.com>
2019-03-24 18:24:07 -0400

    Update TablePartD.py

--- TablePartD.py
+++ TablePartD.py
@@ -3,20 +3,29 @@ import happybase as hb
 # DON'T CHANGE THE PRINT FORMAT, WHICH IS THE OUTPUT
 # OR YOU WON'T RECEIVE POINTS FROM THE GRADER
 
-hero = ???
-power = ???
-name = ???
-xp = ???
-color = ???
+conn = hb.Connection()
+
+table = conn.table('powers')
+
+row = table.row(b'row1')
+hero = row[b'personal:hero']
+power = row[b'personal:power']
+name = row[b'professional:name']
+xp = row[b'professional:xp']
+color = row[b'custom:color']
 
 print('hero: {}, power: {}, name: {}, xp: {}, color: {}'.format(hero, power, name, xp, color))
 
-hero = ???
-color = ???
+row = table.row(b'row19')
+hero = row[b'personal:hero']
+color = row[b'custom:color']
 
 print('hero: {}, color: {}'.format(hero, color))
 
-hero = ???
-name = ???
-color = ???
+row = table.row(b'row1')
+hero = row[b'personal:hero']
+name = row[b'professional:name']
+color = row[b'custom:color']
 print('hero: {}, name: {}, color: {}'.format(hero, name, color))
+
+conn.close()
