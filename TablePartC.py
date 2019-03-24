3f079cba8f228b9337a63a0f5be0a4f2f0a69787
lennyGiannachi <lennygiannachi@gmail.com>
2019-03-24 19:25:27 -0400

    Update TablePartC.py

--- TablePartC.py
+++ TablePartC.py
@@ -5,15 +5,15 @@ conn = hb.Connection()
 table = conn.table('powers')
 row_num = 1
 file_in = open('input.csv')
-for row in file_in:
-        row = row.split(',')
+for record in file_in:
+        row = record.split(',')
         data = {
-                b'personal:hero' : row[1],
-                b'personal:power' : row[2],
-                b'professional:name' : row[3],
-                b'professional:xp' : row[4],
-                b'custom:color' : row[5].strip('/r/n')
+                b'personal:hero' : str.(row[1]),
+                b'personal:power' : str.encode(row[2]),
+                b'professional:name' : str.encode(row[3]),
+                b'professional:xp' : str.encode(row[4]),
+                b'custom:color' : str.encode(row[5].strip('/r/n'))
                }
-        table.put(row[0], data)
+        table.put(str.encode(row[0]), data)
 
 conn.close()
